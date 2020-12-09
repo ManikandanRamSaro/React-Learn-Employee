260a05c3bc26aeaee359680b97bb8e5b3f122437
ManikandanRamSaro <mani95ram@outlook.com>
2020-12-09 22:34:32 +0530

    first commit on 9-12-2020

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+export default class Home extends Component{
+    render()
+    {
+        return(
+            <div className="bg-primary p-5 text-center">
+                    <h1 className="text-white">Welcome to my Home page</h1>
+            </div>
+        );
+    }
+}
