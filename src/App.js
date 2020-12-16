13172cb9aa2d85f15fb09c66838529838befe4d4
ManikandanRamSaro <mani95ram@outlook.com>
2020-12-16 11:20:55 +0530

    After routing and loading static data completed - 16-12-2020

--- src/App.js
+++ src/App.js
@@ -4,25 +4,14 @@ import Button from 'react-bootstrap/Button';
 import Home from './Components/Home';
 import Department from './Components/Department';
 import Employee from './Components/Employee';
+import Navigation from './Components/Navigation';
 import LearningSite from './Components/LearningSite';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-        
-    <nav className="navbar navbar-expand-sm bg-light">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/Department">Department</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/Employee">Employee</a>
-        </li>
-      </ul>
-    </nav>
+        <Navigation/>
+   
     <div className="container">
         <h2 className="text-center">Layout Rendering using React JS </h2>
         <Switch>
@@ -30,6 +19,7 @@ function App() {
             <Route path="/Department" component={Department}  />
             <Route path="/Employee" component={Employee}/>
             <Route path="/learn" component={LearningSite}/>
+            <Route path="/nav" component={Navigation}/>
         </Switch>
     </div>
     </BrowserRouter>
--- src/Components/Department.js
+++ src/Components/Department.js
@@ -1,10 +1,47 @@
 import React,{Component} from 'react';
+import {Table} from 'react-bootstrap';
 export default class Department extends Component{
+
+    constructor(props)
+    {
+        super(props);
+        this.state={arrayofObject:[]};
+    }
+    componentDidMount() // method will executed after all commponents loaded into the application
+    {
+        this.loadStaticData();
+    }
+
+    loadStaticData(){
+        this.setState({
+            arrayofObject:[{"depid":1,"depName":"React"},{"depid":2,"depName":"Angular"},{"depid":3,"depName":"Web API"}]
+        })
+    }
     render()
     {
+        const {arrayofObject} =this.state;
         return(
-            <div className="bg-success p-5 text-center">
+            <div >
+                <div className="bg-success p-5 text-center">
                     <h5 className="text-white">Welcome to my Department page</h5>
+                </div>
+                <Table className="mt-5" stripped size="sm" hover bordered>
+                        <thead>
+                            <tr>
+                                <th>Department ID</th>
+                                <th>Department Name</th>                                
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {arrayofObject.map(dep=>
+                                <tr key="dep.depid">
+                                    <td>{dep.depid}</td>
+                                    <td>{dep.depName}</td>
+                                </tr>
+                            )}
+
+                        </tbody>
+                </Table>
             </div>
         );
     }
